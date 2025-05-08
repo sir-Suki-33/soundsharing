309ba53180c052e5168ca14434c735f6875c0c6e
Suki <sirsuki33@gmail.com>
2025-05-08 22:19:32 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -9,7 +9,11 @@ require('dotenv').config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['https://soundsharing.netlify.app', 'http://localhost:3000'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
